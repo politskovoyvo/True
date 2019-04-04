67495b7a3391c2321eee598d503e144e9c89ecb9
ввва полицвав <vladi19900@yandex.ru>
2019-04-04 23:27:17 +0300

    кеекек

--- NewTestMVVM/ViewModel/MainWindowVM.cs
+++ NewTestMVVM/ViewModel/MainWindowVM.cs
@@ -24,12 +24,16 @@ namespace NewTestMVVM.ViewModel
             AddVisualCommand = new Command(ModelElement.AddVisualView);     //Window 3
 
         }
-
+            
         public Command Comm { get; set; }
         public Command TaskCommand { get; set; }
         public Command AddVisualCommand { get; set; }
         public Model.MyModel ModelElement { get; set; }
 
-       
+        //панель для авторизации первого окна. Login Pass... 
+        public Command EnterLoginCommand { get; set; }
+        public Command NewLoginCommand { get; set; }
+
+
     }
 }
