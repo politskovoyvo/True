b8303786a7ff042e9567af3d7b66c05f36ea4998
ввва полицвав <vladi19900@yandex.ru>
2019-04-05 00:23:07 +0300

    пп

--- NewTestMVVM/View/FirstView.xaml.cs
+++ NewTestMVVM/View/FirstView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using NewTestMVVM.Model;
+using NewTestMVVM.ViewModel;
 
 namespace NewTestMVVM.View
 {
@@ -24,5 +26,6 @@ namespace NewTestMVVM.View
         {
             InitializeComponent();
         }
+        
     }
 }
