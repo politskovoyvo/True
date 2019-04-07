e4105b43930940cc6d0f2e228d7f2669e761b8e1
ввва полицвав <vladi19900@yandex.ru>
2019-04-07 20:36:47 +0300

    newIII

---
+++ NewTestMVVM/ViewModel/FirstLoginWindow/FirstRunViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewTestMVVM.Model;
+
+namespace NewTestMVVM.ViewModel
+{
+    public class FirstRunViewModel : BaseClassElement
+    {
+        public FirstRunViewModel ()
+        {
+            NextView = new Command(AddObject);
+        }
+        private object _contentfirst; 
+        public object ContentFirstRun
+        {
+            get => _contentfirst;
+            set
+            {
+                _contentfirst = value;
+                RaisePropertyChanged("ContentFirstRun");
+            }
+        }
+
+        private void AddObject (object i)
+        {
+            ContentFirstRun = new FirstViewModel(); 
+        }
+        public Command NextView { get; set; } 
+    }
+    
+}
