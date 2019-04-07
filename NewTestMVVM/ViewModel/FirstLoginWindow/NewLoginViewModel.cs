1f61f7d3032456803c28d9e145256d34f74e6de7
ввва полицвав <vladi19900@yandex.ru>
2019-04-07 23:56:23 +0300

    ererer

--- NewTestMVVM/ViewModel/FirstLoginWindow/NewLoginViewModel.cs
+++ NewTestMVVM/ViewModel/FirstLoginWindow/NewLoginViewModel.cs
@@ -11,5 +11,44 @@ namespace NewTestMVVM.ViewModel
 {
     public class NewLoginViewModel : BaseClassElement
     {
+        public NewLoginViewModel () {
+
+        }
+        private string _nameproperty; 
+        public string NameProperty {
+            get => _nameproperty; 
+            set {
+                _nameproperty = value; 
+                RaisePropertyChanged("NameProperty"); 
+            }
+        }
+        private string _fnameproperty; 
+        public string FNameProperty {
+            get => _fnameproperty; 
+            set {
+                _fnameproperty = value;
+                RaisePropertyChanged("FNameProperty");
+            }
+        }
+        private string _profession;
+        public string Profession
+        {
+            get => _profession;
+            set
+            {
+                _profession = value;
+                RaisePropertyChanged("Profession");
+            }
+        }
+        private string _loginproperty;
+        public string LoginProperty
+        {
+            get => _loginproperty;
+            set
+            {
+                _loginproperty = value;
+                RaisePropertyChanged("LoginProperty");
+            }
+        }
     }
 }
